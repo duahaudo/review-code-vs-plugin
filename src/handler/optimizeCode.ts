38affa2c15185465b25fded4b7f82d999c6fbd8a
Stiger <stigerdieu@gmail.com>
2023-08-22 11:04:48 +0700

    feat: update prompt to optimize

--- src/handler/optimizeCode.ts
+++ src/handler/optimizeCode.ts
@@ -2,7 +2,10 @@ import { ExtensionContext } from "vscode";
 import askChatGptHandler from "./base";
 
 const reviewCodeHandler = (context: ExtensionContext) => {
-  return askChatGptHandler(context, `Review and Optimize`);
+  return askChatGptHandler(
+    context,
+    `Optimize (remove complexity, redundant complexity, recursive elimination, improve performance) and give explanation what is better`
+  );
 };
 
 export default reviewCodeHandler;
